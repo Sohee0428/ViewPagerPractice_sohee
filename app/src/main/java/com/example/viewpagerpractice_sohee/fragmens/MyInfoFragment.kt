3f2efd29d9c69fd6c6b1159e5a5c6aad91b3ed1a
Sohee <sohee3480@gmail.com>
2021-05-21 15:51:05 +0900

    뷰페이저에 배치될 3갸의 프래그먼트 완성

---
+++ app/src/main/java/com/example/viewpagerpractice_sohee/fragmens/MyInfoFragment.kt
@@ -0,0 +1,23 @@
+package com.example.viewpagerpractice_sohee.fragmens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.viewpagerpractice_sohee.R
+
+class MyInfoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_info,container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
