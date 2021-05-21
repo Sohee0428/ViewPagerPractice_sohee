dab6a54505721e12661a571488a1b2f1c6305396
Sohee <sohee3480@gmail.com>
2021-05-21 16:30:33 +0900

    코틀린 조건문인 when 활용 예시

--- app/src/main/java/com/example/viewpagerpractice_sohee/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpagerpractice_sohee/adapters/MainViewPagerAdapter.kt
@@ -7,31 +7,47 @@ import com.example.viewpagerpractice_sohee.fragmens.GreetingsFragment
 import com.example.viewpagerpractice_sohee.fragmens.MyInfoFragment
 import com.example.viewpagerpractice_sohee.fragmens.NameFragment
 
-class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
 
-        if(position == 0) {
-            return "이름"
-        }
-        else if (position == 1){
-            return "내 정보"
-        }
-        else {
-            return " 인사말"
+//        자바 조건문
+//        if(position == 0) {
+//            return "이름"
+//        }
+//        else if (position == 1){
+//            return "내 정보"
+//        }
+//        else {
+//            return " 인사말"
+//        }
+
+
+//        코틀린 조건문
+        return when (position){
+            0 -> "이름"
+            1 -> "내 정보"
+            else -> "인사말"
         }
     }
 
     override fun getItem(position: Int): Fragment {
 
-        if(position == 0) {
-            return NameFragment()
-        }
-        else if (position == 1) {
-            return MyInfoFragment()
-        }
-        else {
-            return GreetingsFragment()
+//        자바 조건문
+//        if (position == 0) {
+//            return NameFragment()
+//        } else if (position == 1) {
+//            return MyInfoFragment()
+//        } else {
+//            return GreetingsFragment()
+//        }
+
+
+//        코틀린 조건문
+        return when (position){
+            0 -> NameFragment()
+            1 -> MyInfoFragment()
+            else -> GreetingsFragment()
         }
 
     }
